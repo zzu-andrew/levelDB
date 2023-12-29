5a1983e99c53a40f18dbf6e66dea42aba23a9565
zzu-andrew <564631192@qq.com>
2023-12-29 18:16:23 +0800

    编写序列化文档

--- demo/batch_test/write_batch_demo.h
+++ demo/batch_test/write_batch_demo.h
@@ -13,7 +13,6 @@
 
 /*
  * 对write_bach 进行重新实现，一般理解里面的实现原理
- *
  * */
 namespace WRITE_BATCH_DEMO {
 
@@ -33,36 +32,24 @@ namespace WRITE_BATCH_DEMO {
 
         WriteBatch();
 
-        // Intentionally copyable.
+        // 支持copy构造函数
         WriteBatch(const WriteBatch &) = default;
-
+        // 支持赋值操作
         WriteBatch &operator=(const WriteBatch &) = default;
 
         ~WriteBatch();
 
-        // Store the mapping "key->value" in the database.
+        // Put 将Key-value形式的值存储在数据库中
         void Put(const leveldb::Slice &key, const leveldb::Slice &value);
-
-        // If the database contains a mapping for "key", erase it.  Else do nothing.
+        // 当某个值不想要时,按照key值将对应的键值对删除
         void Delete(const leveldb::Slice &key);
-
-        // Clear all updates buffered in this batch.
+        // 将 batch中所有的缓存清理，并预留出长度和序列号字段足够的长度
         void Clear();
-
-        // The size of the database changes caused by this batch.
-        //
-        // This number is tied to implementation details, and may change across
-        // releases. It is intended for LevelDB usage metrics.
+        // 获取batch内部指标数据长度，也就是获取rep_的大小
         size_t ApproximateSize() const;
-
-        // Copies the operations in "source" to this batch.
-        //
-        // This runs in O(source size) time. However, the constant factor is better
-        // than calling Iterate() over the source batch with a Handler that replicates
-        // the operations into this batch.
+        // 当有多个batch想合并时，可以使用append将batch添加到另外衣蛾batch后面
         void Append(const WriteBatch &source);
-
-        // Support for iterating over the contents of a batch.
+        // 对batch进行迭代，这样能够按照类型取出所有batch里面的值
         leveldb::Status Iterate(Handler *handler) const;
 
     private:
